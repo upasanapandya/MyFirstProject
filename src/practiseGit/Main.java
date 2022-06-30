8a41e76e4e6700aed11561e004558785d73a922f
upasanapandya <upasanapandya0306@gmail.com>
2022-06-30 16:45:51 -0400

    printed natural numbers

--- src/practiseGit/Main.java
+++ src/practiseGit/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World" );
         System.out.println("Hi");
+        for(int i = 0 ; i < 10 ; i ++)
+        {
+            System.out.println("Print natural  numbers" + i);
+
+        }
     }
 }
